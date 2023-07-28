9b913a76c60c1531f3b222c78fdf48fe3c103ed1
maggick <maggick@users.noreply.github.com>
2023-07-28 10:39:15 +0200

    `redirect_uri` as a global variable

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -1,13 +1,16 @@
+const redirect_uri = "https://maggick.fr/Spotify_RAS/callback";
+
 export async function redirectToAuthCodeFlow(clientId: string) {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
+
 
     localStorage.setItem("verifier", verifier);
 
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", redirect_uri);
     params.append("scope", "user-library-read user-modify-playback-state");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -22,7 +25,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://maggick.fr/Spotify_RAS/callback");
+    params.append("redirect_uri", redirect_uri);
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
