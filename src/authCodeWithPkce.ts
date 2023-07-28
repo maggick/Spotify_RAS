9aef0e62fd2794a3409b06b07f9af44ce1b3b2fc
maggick <maggick@users.noreply.github.com>
2023-07-28 10:32:54 +0200

    Fix redirect uri

--- src/authCodeWithPkce.ts
+++ src/authCodeWithPkce.ts
@@ -22,7 +22,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://maggick.fr/Spotify_RAS/callback");
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
