306c379b67f7a7c9b034899e539c56b63d557f2b
maggick <maggick@users.noreply.github.com>
2023-08-01 20:41:40 +0200

    Remove personnalized theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,6 @@ export default {
     require("daisyui")
   ],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#e0a562",
-          "secondary": "#540589",
-          "accent": "#f22e31",
-          "neutral": "#27293a",
-          "base-100": "#324453",
-          "info": "#6487d8",
-          "success": "#105b4b",
-          "warning": "#eca241",
-          "error": "#e34557",
-          },
-        },
-      ],
+    themes: false,
     },
 }
